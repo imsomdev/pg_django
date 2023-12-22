87ea00594d282a17b2a7fb879671f53c706c69f4
Somdev Das <somdev4g@gmail.com>
2023-12-22 14:15:55 +0530

    Project is Ready for Work

---
+++ pgdb_test/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=240)
+    email = models.EmailField()
+    created = models.DateField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.name
