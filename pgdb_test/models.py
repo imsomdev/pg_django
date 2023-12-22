29b7c663e17d6a7c6284b675e0d840f63b4e9396
Somdev Das <somdev4g@gmail.com>
2023-12-22 15:55:01 +0530

    made emial field unique

--- pgdb_test/models.py
+++ pgdb_test/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Customer(models.Model):
     name = models.CharField(max_length=240)
-    email = models.EmailField()
+    email = models.EmailField(unique=True)
     created = models.DateField(auto_now_add=True)
 
 
