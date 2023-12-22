59c810e895e7e1b91e99721b8b8aef6a96425c97
Somdev Das <somdev4g@gmail.com>
2023-12-22 15:59:20 +0530

    Authentication Added

--- pgdb_test/views.py
+++ pgdb_test/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from .models import Customer
 from .serializers import CustomerSerializer
 
 class CustomerViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticatedOrReadOnly]
     serializer_class = CustomerSerializer
     queryset = Customer.objects.all()
